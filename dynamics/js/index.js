1330bd5ae47e1ce485189d91fd5da7e8a59f4286
Rommelk965 <romelkhh@gmail.com>
2022-05-29 01:56:23 -0500

    fix: Peticion de personajes

--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -1,56 +1,35 @@
 window.addEventListener("load", () =>{
     const opciones = document.getElementById("opciones");
     const texto = document.getElementById("texto");
-    const nombre = document.getElementById("nombre");
+    const nombres = document.getElementById("nombre");
     const genero = document.getElementById("genero");
     const descripcion = document.getElementById("descripcion");
     const publicacion = document.getElementById("publicacion");
     const personajes = document.getElementById("personajes");
-    // const myHero = document.getElementById("0") //los id son numeros que corresponden al id en las tablas 
-    // const violetEv = document.getElementById("1")
-    // const deathNote = document.getElementById("2")
-    // const spyX = document.getElementById("3")
-    // const tenseiS = document.getElementById("4")
 
     /*INTENTO DE HACER QUE DEPENDIENDO DEL ANIME SE MUESTRE LA INFORMACION*/
     opciones.addEventListener("click", (evento)=>{
         const anime_elegido = evento.target; 
         const id_anime = anime_elegido.id;
+        console.log(id_anime);
         fetch("dynamics/php/anime.php?id="+ id_anime)
         .then((response)=>{
             return response.json();
         })
         .then((datosJSON)=>{
             if(datosJSON){
+                console.log(datosJSON);
+                nombres.innerHTML = "Nombre:<div class='info'>"+datosJSON.nombre+"</div>";
+                genero.innerHTML = "Genero:<div class='info'>"+datosJSON.genero+"</div>";
+                descripcion.innerHTML = "Descripción:<div class='info'>"+datosJSON.descripcion+"</div>";
+                publicacion.innerHTML = "Publicación:<div class='info'>"+datosJSON.publicacion+"</div>";
+                personajes.innerHTML = "Personajes: <div class='info'>"+datosJSON.personajes+"</div>"
                 texto.style.display = "block";
-                // nombre.innerHTML = "";
             }
         });
         
         
     })
-
-    //profe
-    /*divResultados.addEventListener("click", (evento)=>{
-        if(evento.target.classList.contains("coincidencia")){
-        let id = evento.target.dataset.id;
-
-        fetch("dynamics/php/pokemon.php?id="+id)
-            .then((response)=>{
-            return response.json();
-            })
-            .then((datosJSON)=>{
-            if(datosJSON.ok == true){
-                divDatos.innerHTML="<div class='dato'><strong>Nombre</strong>"+datosJSON.datos.nombre+"</div>";
-                divDatos.innerHTML+="<div class='dato'><strong>Altura</strong>"+datosJSON.datos.altura+"</div>";
-                divDatos.innerHTML+="<div class='dato'><strong>Peso</strong>"+datosJSON.datos.peso+"</div>";
-                divDatos.innerHTML+="<div class='dato'><strong>Tipo</strong>"+datosJSON.datos.tipo+"</div>";
-                divDatos.innerHTML+= "<button data-id=" + id + "id='btn-eliminar'>Eliminar pokemon</button>";
-                divDatos.style.display = "flex";
-            }
-            });
-        }
-    });*/
 
 });
 
--- dynamics/php/anime.php
+++ dynamics/php/anime.php
@@ -9,17 +9,41 @@ if(!$con)
 }
 else
 {
-    //echo "si se pudo conectar";
-    $id=(isset($_POST['id']) && $_POST["id"] != "")? $_POST['id'] : false;
+    $id=(isset($_GET['id']) && $_GET["id"] != "")? $_GET['id'] : false;
 
-    $sql = "SELECT id_anime, nombre, descripcion, publicacion FROM anime WHERE id_anime = '$id'";
-    $res = mysqli_query($con, $sql);
+    $sql = "SELECT 
+            id_anime, nombre, descripcion, publicacion, genero 
+            FROM anime 
+            NATURAL JOIN genero
+            WHERE id_anime = '$id'
+          ";
+    $res1 = mysqli_query($con, $sql);
+
+    $sql = "SELECT id_personaje, genero, nombre, alias, edad, estado, interesAmoroso, id_anime FROM personaje NATURAL JOIN animehaspersonaje WHERE id_anime=$id";
+
+    $res2 = mysqli_query($con, $sql);
 
     $resultados = [];
-
-    if($res == true){
-      $row = mysqli_fetch_assoc($res);
-      $resultados[] = $row;
+    
+    // var_dump($res1, $res2);
+    if($res1 == true && $res2 == true){
+      $row1 = mysqli_fetch_assoc($res1);
+      $row2 = mysqli_fetch_assoc($res2);
+      $datos = array(
+                    "id" => $row1["id_anime"],
+                    "nombre" => $row1["nombre"],
+                    "descripcion" => $row1["descripcion"],
+                    "publicacion" => $row1["publicacion"], 
+                    "genero" => $row1["genero"],
+                    "idP" => $row2["id_personaje"],
+                    "generoP" => $row2["genero"],
+                    "nombreP" => $row2["nombre"],
+                    "alias" => $row2["alias"],
+                    "edad" => $row2["edad"],
+                    "estado" => $row2["estado"], 
+                    "interesAmoroso" => $row2["interesAmoroso"]
+                  );       
+        $resultados = $datos;
     }else{
       echo mysqli_error($con);
     }
