2f7005f555995163c45ca658eb67493105ec3718
Aranza747 <aranzaromero@comunidad.unam.mx>
2022-05-28 19:32:20 -0500

    feat(index.html):correcciones

--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -11,23 +11,44 @@ const tenseiS = document.getElementById("4")
 /*INTENTO DE HACER QUE DEPENDIENDO DEL ANIME SE MUESTRE LA INFORMACION*/
 myHero.addEventListener("click", (evento)=>{
     texto.style.display = "block";
-   // console.log(1)
+    console.log(1)
     console.log(myHero)
-    fetch("dynamics/php/anime.php?id="+id)
-    // fetch("dynamics/php/anime.php")
-    // .then((response)=>{
-    // return response.json();
-    // })
-    // .then((datosJSON)=>{
-    // console.log(datosJSON);
-    // //let selectTipos = document.getElementById("select-tipos");
-    // for(tipo of datosJSON){
-    //     console.log("si")
-    //     // selectTipos.innerHTML+="<option value='"+tipo.id+"'>"+tipo.nombre+"</option>";
-    // }
-    // });
-
+    fetch("dynamics/php/anime.php?id="+1)
+    fetch("dynamics/php/anime.php")
+    .then((response)=>{
+        return response.json();
+    })
+    .then((datosJSON)=>{
+        console.log(datosJSON);
+        let selectTipos = document.getElementById("select-tipos");
+        for(tipo of datosJSON){
+            console.log("si")
+            selectTipos.innerHTML+="<option value='"+tipo.id+"'>"+tipo.nombre+"</option>";
+        }
+    });
 })
+
+//profe
+divResultados.addEventListener("click", (evento)=>{
+    if(evento.target.classList.contains("coincidencia")){
+      let id = evento.target.dataset.id;
+
+      fetch("dynamics/php/pokemon.php?id="+id)
+        .then((response)=>{
+          return response.json();
+        })
+        .then((datosJSON)=>{
+          if(datosJSON.ok == true){
+            divDatos.innerHTML="<div class='dato'><strong>Nombre</strong>"+datosJSON.datos.nombre+"</div>";
+            divDatos.innerHTML+="<div class='dato'><strong>Altura</strong>"+datosJSON.datos.altura+"</div>";
+            divDatos.innerHTML+="<div class='dato'><strong>Peso</strong>"+datosJSON.datos.peso+"</div>";
+            divDatos.innerHTML+="<div class='dato'><strong>Tipo</strong>"+datosJSON.datos.tipo+"</div>";
+            divDatos.innerHTML+= "<button data-id=" + id + "id='btn-eliminar'>Eliminar pokemon</button>";
+            divDatos.style.display = "flex";
+          }
+        });
+    }
+  });
 
 
 
